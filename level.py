b97be1b87f69c543f10af8f23868e4889c03caed
Alex King <calicoalix@gmail.com>
2022-08-04 09:59:12 +1000

    init py rpg

---
+++ level.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Level:
+    def __init__(self):
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+    # __init__()
+    def run(self):
+        pass
+    # run()
---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame, sys
+from settings import * 
+from level import Level
+
+class Game:
+    def __init__(self):
+        window_title = 'Hi Ro :P'
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(window_title)
+        self.clock = pygame.time.Clock() 
+
+        self.level = Level()
+    # init()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                self.screen.fill('black')
+                self.level.run()
+                pygame.display.update()
+                self.clock.tick(FPS)
+    # run()
+# Game:
+if __name__ == '__main__': # check if main file
+    game = Game() # create instance of class
+    game.run() # call run method in class
