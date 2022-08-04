2ec7cbbe7a02667bd3f84c5ecdcbd5f025659f12
Alex King <calicoalix@gmail.com>
2022-08-04 16:31:36 +1000

    fixed overlap

--- code/tile.py
+++ code/tile.py
@@ -5,5 +5,11 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))):
         super().__init__(groups)
         self.image = surface
-        self.rect = self.image.get_rect(topleft=pos)
+        if sprite_type == 'object': 
+            self.rect = self.image.get_rect(topleft = (pos[0], pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
+    # init()
+
+# Tile()
