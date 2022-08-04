85f9bcd9887e0cfc42ec34da60f9fbb16b0dc7ef
Alex King <calicoalix@gmail.com>
2022-08-04 10:56:50 +1000

    move works and horizontals are normalized

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -20,8 +21,13 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
+
     # create_map()
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
     # run()
--- code/player.py
+++ code/player.py
@@ -7,3 +7,36 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
 
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+    # init()
+
+    def input(self):
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else: 
+            self.direction.y = 0
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else: 
+            self.direction.x = 0
+    # input()
+
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+            
+        self.rect.center += self.direction * speed
+    # move()
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
+    # update
+
