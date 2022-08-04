6c8db386dfbc25bc17c9ae52d26ba778463ecd70
Alex King <calicoalix@gmail.com>
2022-08-04 14:08:48 +1000

    z-index works with hitbox

--- code/level.py
+++ code/level.py
@@ -27,14 +27,31 @@ class Level:
 
     # create_map()
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+        # debug(self.player.direction)
     # run()
-
+# Level
 
 class YSortCameraGroup(pygame.sprite.Group): 
     def __init__(self):
-        super().__init__()
 
-# YSortCameraGroup()
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    # __init__()
+
+    def custom_draw(self, player):
+
+        self.offset.x = player.rect.centerx - self.half_width 
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+    # custom_draw()
+
+# YSortCameraGroup
