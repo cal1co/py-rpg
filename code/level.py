20bdf7813bbb1863c84dcc3bc077c1efe71a6ce9
Alex King <calicoalix@gmail.com>
2022-08-04 10:14:43 +1000

    level print in console

--- code/level.py
+++ code/level.py
@@ -1,12 +1,21 @@
 import pygame
+from settings import *
 
 class Level:
     def __init__(self):
 
         self.display_surface = pygame.display.get_surface 
+        
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        self.create_map()
+
     # __init__()
+    def create_map(self):
+        for row in WORLD_MAP:
+            print(row)
+    # create_map()
     def run(self):
         pass
     # run()
