0eb04d10b46d7ad605d39bf39c872ea6888993f7
Alex King <calicoalix@gmail.com>
2022-08-08 18:00:37 +1000

    init enemies

--- code/level.py
+++ code/level.py
@@ -7,6 +7,7 @@ from support import import_csv_layout, import_folder
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
     def __init__(self):
@@ -27,6 +28,7 @@ class Level:
             'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
             'grass': import_csv_layout('../map/map_Grass.csv'),
             'object': import_csv_layout('../map/map_Objects.csv'),
+            'entities': import_csv_layout('../map/map_Entities.csv')
         }
         graphics = {
             'grass': import_folder('../graphics/Grass'),
@@ -48,16 +50,25 @@ class Level:
                         if style == 'object':
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+                        if style == 'entities':
+                            if col == '394':
+                                self.player = Player(
+                                    (x, y), 
+                                    [self.visible_sprites], 
+                                    self.obstacle_sprites, 
+                                    self.create_attack, 
+                                    self.destroy_attack,
+                                    self.create_magic
+                                )
+                            else:
+                                if col == '390': monster_name = 'bamboo'
+                                elif col == '391': monster_name = 'spirit'
+                                elif col == '392': monster_name = 'raccoon'
+                                else: monster_name = 'squid'
 
-        self.player = Player(
-            (2000, 1430), 
-            [self.visible_sprites], 
-            self.obstacle_sprites, 
-            self.create_attack, 
-            self.destroy_attack,
-            self.create_magic
-        )
-    # create_map()
+                                Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacle_sprites)
+                                
+    # create_map() 
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites])
@@ -78,6 +89,7 @@ class Level:
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
         self.ui.display(self.player) 
         # debug(self.player.status)
     # run()
@@ -107,4 +119,10 @@ class YSortCameraGroup(pygame.sprite.Group):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
     # custom_draw()
+
+    def enemy_update(self, player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
+    # enemy_update()
 # YSortCameraGroup
--- code/player.py
+++ code/player.py
@@ -1,8 +1,9 @@
 import pygame 
 from settings import *
 from support import import_folder
+from entity import Entity
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
     def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
@@ -11,10 +12,7 @@ class Player(pygame.sprite.Sprite):
 
         self.import_player_asssets()
         self.status = 'down'
-        self.frame_index = 0
-        self.animation_speed = 0.15
 
-        self.direction = pygame.math.Vector2()
         self.attacking = False
         self.attack_cooldown = 400 
         self.attack_time = None
@@ -138,34 +136,6 @@ class Player(pygame.sprite.Sprite):
             if 'attack' in self.status:
                 self.status = self.status.replace('_attack', '')
     # get_status()
-
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center 
-    # move()
-
-    def collision(self, direction):
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0:
-                        self.hitbox.right = sprite.hitbox.left 
-                    if self.direction.x < 0:
-                        self.hitbox.left = sprite.hitbox.right 
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0:
-                        self.hitbox.bottom = sprite.hitbox.top 
-                    if self.direction.y < 0:
-                        self.hitbox.top = sprite.hitbox.bottom
-    # collision()
 
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
